782294763370e92c3ff3823549f048dad0a65905
LinChen0311 <71689299+LinChen0311@users.noreply.github.com>
2020-10-20 15:43:38 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,67 @@
+led.plot(4, 0)
+basic.pause(100)
+let x = 2
+let y = -1
+for (let index = 0; index < 2; index++) {
+    x = x + 1
+    y = y + 1
+    led.plot(x, y)
+    basic.pause(100)
+}
+x = 1
+y = -1
+for (let index = 0; index < 3; index++) {
+    x = x + 1
+    y = y + 1
+    led.plot(x, y)
+    basic.pause(100)
+}
+x = 0
+y = -1
+for (let index = 0; index < 4; index++) {
+    x = x + 1
+    y = y + 1
+    led.plot(x, y)
+    basic.pause(100)
+}
+x = -1
+y = -1
+for (let index = 0; index < 5; index++) {
+    x = x + 1
+    y = y + 1
+    led.plot(x, y)
+    basic.pause(100)
+}
+x = -1
+y = 0
+for (let index = 0; index < 4; index++) {
+    x = x + 1
+    y = y + 1
+    led.plot(x, y)
+    basic.pause(100)
+}
+x = -1
+y = 1
+for (let index = 0; index < 3; index++) {
+    x = x + 1
+    y = y + 1
+    led.plot(x, y)
+    basic.pause(100)
+}
+x = -1
+y = 2
+for (let index = 0; index < 2; index++) {
+    x = x + 1
+    y = y + 1
+    led.plot(x, y)
+    basic.pause(100)
+}
+x = -1
+y = 3
+for (let index = 0; index < 2; index++) {
+    x = x + 1
+    y = y + 1
+    led.plot(x, y)
+    basic.pause(100)
+}
+led.plot(0, 4)
